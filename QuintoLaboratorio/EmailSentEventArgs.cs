3055eca10b635e8bfc3b1fbdacd06229e90a0467
Lucas Moyano Rivas <lucasmoyanor@Lucass-MacBook-Pro-2.local>
2020-04-26 20:21:03 -0400

    listo 1/4 del lab5

---
+++ QuintoLaboratorio/EmailSentEventArgs.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace QuintoLaboratorio
+{
+    public class EmailSentEventArgs : EventArgs
+    {
+        public string Email { get; set; }
+        public string Username { get; set; }
+    }
+}
